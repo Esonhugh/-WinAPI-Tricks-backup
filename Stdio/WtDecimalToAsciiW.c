35ae9008d08e38df2ff71f1895a5f4cdb43598e4
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-02-20 22:17:47 -0600

    Add files via upload

---
+++ Stdio/WtDecimalToAsciiW.c
@@ -0,0 +1,11 @@
+DWORD WtDecimalToAsciiW(PWCHAR String, LPDWORD dwArray, DWORD dwLength)
+{
+	DWORD dwX = ERROR_SUCCESS;
+
+	if (String == NULL)
+		return dwX;
+
+	for (; dwX < dwLength; dwX++) { String[dwX] = (WCHAR)dwArray[dwX]; }
+
+	return dwX;
+}
