1d37746eb908a43aca464d3021aaf85817099382
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-02-20 20:40:02 -0600

    Update GetPebAlt.c

--- GetPebAlt.c
+++ GetPebAlt.c
@@ -7,7 +7,6 @@ PPEB GetPebAlt(VOID)
 #if defined(_WIN64)
 	PTEB Teb = (PTEB)__readgsqword(0x30);
 #elif define(_WIN32)
-	//32bit
 	PTEB Teb = (PTEB)__readfsdword(0x18);
 #endif
 	return (PPEB)Teb->ProcessEnvironmentBlock;
