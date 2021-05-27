65b874a5eec73f8477b4f5bbf1a1c54918117a4d
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-27 11:40:28 -0500

    Update and rename WtGetFullPath.c to GetFullPath.c

--- Kernel32/GetFullPath.c
+++ Kernel32/GetFullPath.c
@@ -348,7 +348,7 @@ PPEB GetPeb(VOID)
 #endif
 }
 
-PWCHAR WtStringCopyW(PWCHAR String1, PWCHAR String2)
+PWCHAR StringCopyW(PWCHAR String1, PWCHAR String2)
 {
 	PWCHAR p = String1;
 
@@ -368,7 +368,7 @@ INT main(VOID)
 	Peb = (PPEB)GetPeb();
 	ProcessParameters = Peb->ProcessParameters;
 
-	WtStringCopyW(FullPath, ProcessParameters->ImagePathName.Buffer);
+	StringCopyW(FullPath, ProcessParameters->ImagePathName.Buffer);
 
 	return ERROR_SUCCESS;
 }
