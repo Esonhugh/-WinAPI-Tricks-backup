e14613a1826974f196d9d76dd1800354a38bd7d4
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-27 12:00:41 -0500

    Update IsPathValid.c

--- Kernel32/IsPathValid.c
+++ Kernel32/IsPathValid.c
@@ -1,8 +1,22 @@
-BOOL IsPathValid(PWCHAR FilePath)
+BOOL IsPathValidW(PWCHAR FilePath)
 {
     HANDLE hFile = INVALID_HANDLE_VALUE;
 
     hFile = CreateFileW(FilePath, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE)
+        return FALSE;
+		
+    if(hFile)
+	CloseHandle(hFile);
+
+    return TRUE;
+}
+
+BOOL IsPathValidA(PCHAR FilePath)
+{
+    HANDLE hFile = INVALID_HANDLE_VALUE;
+
+    hFile = CreateFileA(FilePath, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hFile == INVALID_HANDLE_VALUE)
         return FALSE;
 		
