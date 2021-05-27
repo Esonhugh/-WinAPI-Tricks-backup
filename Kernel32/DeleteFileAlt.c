009256d704b8d9afe21d9af6b1f54ca32abddeab
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-27 12:00:55 -0500

    Add files via upload

---
+++ Kernel32/DeleteFileAlt.c
@@ -0,0 +1,39 @@
+BOOL DeleteFileAltW(PWCHAR Path)
+{
+	HANDLE hHandle = INVALID_HANDLE_VALUE;
+
+	if (Path == NULL)
+		return FALSE;
+
+	if (!IsPathValidW(Path))
+		return FALSE;
+
+	hHandle = CreateFileW(Path, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_FLAG_DELETE_ON_CLOSE, NULL);
+	if (hHandle == INVALID_HANDLE_VALUE)
+		return FALSE;
+
+	if (hHandle)
+		CloseHandle(hHandle);
+
+	return TRUE;
+}
+
+BOOL DeleteFileAltA(PWCHAR Path)
+{
+	HANDLE hHandle = INVALID_HANDLE_VALUE;
+
+	if (Path == NULL)
+		return FALSE;
+
+	if (!IsPathValidA(Path))
+		return FALSE;
+
+	hHandle = CreateFileA(Path, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_FLAG_DELETE_ON_CLOSE, NULL);
+	if (hHandle == INVALID_HANDLE_VALUE)
+		return FALSE;
+
+	if (hHandle)
+		CloseHandle(hHandle);
+
+	return TRUE;
+}
