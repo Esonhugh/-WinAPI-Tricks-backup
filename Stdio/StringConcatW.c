7894d9e3628fa0e549efdaa50811e6256a68e086
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-27 11:49:05 -0500

    Update StringConcatW.c

--- Stdio/StringConcatW.c
+++ Stdio/StringConcatW.c
@@ -1,4 +1,4 @@
-SIZE_T StringLength(LPCWSTR String)
+SIZE_T StringLengthW(LPCWSTR String)
 {
 	LPCWSTR String2;
 
@@ -19,6 +19,34 @@ PWCHAR StringCopyW(PWCHAR String1, PWCHAR String2)
 PWCHAR StringConcatW(PWCHAR String, PWCHAR String2)
 {
 	StringCopyW(&String[StringLength(String)], String2);
+
+	return String;
+}
+
+
+//
+
+SIZE_T StringLengthA(LPCSTR String)
+{
+	LPCSTR String2;
+
+	for (String2 = String; *String2; ++String2);
+
+	return (String2 - String);
+}
+
+PWCHAR StringCopyA(PCHAR String1, PCHAR String2)
+{
+	PCHAR p = String1;
+
+	while ((*p++ = *String2++) != 0);
+
+	return String1;
+}
+
+PWCHAR StringConcatA(PCHAR String, PCHAR String2)
+{
+	StringCopyA(&String[StringLengthA(String)], String2);
 
 	return String;
 }
