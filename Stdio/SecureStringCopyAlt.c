fd3ba63b3cea91784bed2e9da7ea9c7aa4c1b4b7
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-27 11:47:36 -0500

    Update SecureStringCopyAlt.c

--- Stdio/SecureStringCopyAlt.c
+++ Stdio/SecureStringCopyAlt.c
@@ -1,6 +1,15 @@
-PWCHAR SecureStringCopyAlt(PWCHAR String1, LPCWSTR String2, SIZE_T Size)
+PWCHAR SecureStringCopyAltW(PWCHAR String1, LPCWSTR String2, SIZE_T Size)
 {
 	PWCHAR pChar = String1;
+
+	while (Size-- && (*String1++ = *String2++) != '\0');
+
+	return pChar;
+}
+
+PCHAR SecureStringCopyAltA(PCHAR String1, LPCSTR String2, SIZE_T Size)
+{
+	PCHAR pChar = String1;
 
 	while (Size-- && (*String1++ = *String2++) != '\0');
 
