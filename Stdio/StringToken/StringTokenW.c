45d7720d248d3350878fbfd7c3d6ae24fef278ff
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-28 01:22:23 -0500

    Update and rename Stdio/StringToken.c to Stdio/StringToken/StringTokenW.c

---
+++ Stdio/StringToken/StringTokenW.c
@@ -0,0 +1,45 @@
+PWCHAR StringTokenW(PWCHAR String, CONST PWCHAR Delim)
+{
+	PWCHAR Last;
+	PWCHAR SpanP, Token;
+	INT C, SC;
+
+	if (String == NULL)
+		return NULL;
+
+CONTINUE:
+
+	C = *String++;
+
+	for (SpanP = (PWCHAR)Delim; (SC = *SpanP++) != ERROR_SUCCESS;)
+	{
+		if (C == SC)
+			goto CONTINUE;
+	}
+
+	if (C == ERROR_SUCCESS) { Last = NULL; return NULL; }
+
+	Token = String - 1;
+
+	for (;;)
+	{
+		C = *String++;
+		SpanP = (PWCHAR)Delim;
+
+		do {
+			if ((SC = *SpanP++) == C)
+			{
+				if (C == ERROR_SUCCESS)
+					String = NULL;
+				else
+					String[-1] = '\0';
+
+				Last = String;
+				return Token;
+			}
+		} while (SC != ERROR_SUCCESS);
+	}
+
+	return NULL;
+
+}
