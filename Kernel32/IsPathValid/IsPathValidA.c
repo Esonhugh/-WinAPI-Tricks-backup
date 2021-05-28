983e038035b5be73e24d24795cd31b1c036ed6a8
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-28 12:43:50 -0500

    Add files via upload

---
+++ Kernel32/IsPathValid/IsPathValidA.c
@@ -0,0 +1,13 @@
+BOOL IsPathValidA(PCHAR FilePath)
+{
+    HANDLE hFile = INVALID_HANDLE_VALUE;
+
+    hFile = CreateFileA(FilePath, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE)
+        return FALSE;
+		
+    if(hFile)
+	CloseHandle(hFile);
+
+    return TRUE;
+}
