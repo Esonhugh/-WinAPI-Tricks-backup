fb0301a4e8f6a1b28260134df4f9096c5297a904
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-28 00:57:30 -0500

    Update CaplockStringA.c

--- Stdio/CaplockString/CaplockStringA.c
+++ Stdio/CaplockString/CaplockStringA.c
@@ -1,16 +1,3 @@
-PWCHAR CaplockStringW(PWCHAR Ptr)
-{
-	PWCHAR sv = Ptr;
-	while (*sv != '\0')
-	{
-		if (*sv >= 'a' && *sv <= 'z')
-			*sv = *sv - ('a' - 'A');
-
-		sv++;
-	}
-	return Ptr;
-}
-
 PCHAR CaplockStringA(PCHAR Ptr)
 {
 	PCHAR sv = Ptr;
