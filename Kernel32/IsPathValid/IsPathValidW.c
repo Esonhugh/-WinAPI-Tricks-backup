69ba225b2931fd60914438bc75d9cfd308d591fb
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-28 12:43:42 -0500

    Update and rename Kernel32/IsPathValid.c to Kernel32/IsPathValid/IsPathValidW.c

--- Kernel32/IsPathValid/IsPathValidW.c
+++ Kernel32/IsPathValid/IsPathValidW.c
@@ -12,16 +12,4 @@ BOOL IsPathValidW(PWCHAR FilePath)
     return TRUE;
 }
 
-BOOL IsPathValidA(PCHAR FilePath)
-{
-    HANDLE hFile = INVALID_HANDLE_VALUE;
 
-    hFile = CreateFileA(FilePath, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE)
-        return FALSE;
-		
-    if(hFile)
-	CloseHandle(hFile);
-
-    return TRUE;
-}
