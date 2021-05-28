bb5a2f78864ae12ae67b0e46bcc36a19e6f0cd91
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-28 01:07:05 -0500

    Update and rename Stdio/StringCompare.c to Stdio/StringCompare/StringCompareA.c

---
+++ Stdio/StringCompare/StringCompareA.c
@@ -0,0 +1,10 @@
+INT StringCompareA(LPCSTR String1, LPCSTR String2)
+{
+	for (; *String1 == *String2; String1++, String2++)
+	{
+		if (*String1 == '\0')
+			return 0;
+	}
+
+	return ((*(LPCSTR)String1 < *(LPCSTR)String2) ? -1 : +1);
+}
