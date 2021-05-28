b31de23240ae37184a1f036a1d2e5b1f79004e59
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-28 01:11:05 -0500

    Update and rename Stdio/StringCopy.c to Stdio/StringCopy/StringCopyW.c

--- Stdio/StringCopy/StringCopyW.c
+++ Stdio/StringCopy/StringCopyW.c
@@ -6,12 +6,3 @@ PWCHAR StringCopyW(PWCHAR String1, PWCHAR String2)
 
 	return String1;
 }
-
-PCHAR StringCopyA(PCHAR String1, PCHAR String2)
-{
-	PCHAR p = String1;
-
-	while ((*p++ = *String2++) != 0);
-
-	return String1;
-}
