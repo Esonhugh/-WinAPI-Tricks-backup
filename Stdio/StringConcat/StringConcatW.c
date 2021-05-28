2c025e847d5e609ff527440add86364fde4a5202
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-28 01:10:09 -0500

    Add files via upload

---
+++ Stdio/StringConcat/StringConcatW.c
@@ -0,0 +1,24 @@
+SIZE_T StringLengthW(LPCWSTR String)
+{
+	LPCWSTR String2;
+
+	for (String2 = String; *String2; ++String2);
+
+	return (String2 - String);
+}
+
+PWCHAR StringCopyW(PWCHAR String1, PWCHAR String2)
+{
+	PWCHAR p = String1;
+
+	while ((*p++ = *String2++) != 0);
+
+	return String1;
+}
+
+PWCHAR StringConcatW(PWCHAR String, PWCHAR String2)
+{
+	StringCopyW(&String[StringLength(String)], String2);
+
+	return String;
+}
