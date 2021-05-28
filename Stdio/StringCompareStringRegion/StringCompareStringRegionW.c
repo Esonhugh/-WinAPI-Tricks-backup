793086801137fbd7c798f573065c9177f5936baa
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-28 01:08:37 -0500

    Update and rename Stdio/StringCompareStringRegion.c to Stdio/StringCompareStringRegion/StringCompareStringRegionW.c

---
+++ Stdio/StringCompareStringRegion/StringCompareStringRegionW.c
@@ -0,0 +1,17 @@
+INT StringCompareStringRegionW(PWCHAR String1, PWCHAR String2, SIZE_T Count)
+{
+	UCHAR Block1, Block2;
+	while (Count-- > 0)
+	{
+		Block1 = (UCHAR)*String1++;
+		Block2 = (UCHAR)*String2++;
+
+		if (Block1 != Block2)
+			return Block1 - Block2;
+
+		if (Block1 == '\0')
+			return 0;
+	}
+
+	return 0;
+}
