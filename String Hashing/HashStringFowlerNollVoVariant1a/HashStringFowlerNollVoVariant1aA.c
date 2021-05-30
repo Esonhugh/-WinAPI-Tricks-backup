16a961e3e38521dac9c749b2f9a9ee396260a34e
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-29 20:44:25 -0500

    Add files via upload

---
+++ String Hashing/HashStringFowlerNollVoVariant1a/HashStringFowlerNollVoVariant1aA.c
@@ -0,0 +1,12 @@
+ULONG HashStringFowlerNollVoVariant1aA(PCHAR String)
+{
+	ULONG Hash = 0x811c9dc5;
+
+	while (*String)
+	{
+		Hash ^= (UCHAR)*String++;
+		Hash *= 0x01000193;
+	}
+
+	return Hash;
+}
