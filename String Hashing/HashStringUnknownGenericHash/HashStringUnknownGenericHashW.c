b9d076a7b1f59f25624df60bf35c53d7b635b118
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-05-30 21:39:04 -0500

    Add files via upload

---
+++ String Hashing/HashStringUnknownGenericHash/HashStringUnknownGenericHashW.c
@@ -0,0 +1,19 @@
+INT HashStringUnknownGenericHashW(PWCHAR String)
+{
+	PWCHAR Pointer;
+	INT Generic;
+	INT Hash = 0;
+
+	for (Pointer = String; *Pointer != '\0'; Pointer++)
+	{
+		Hash = (Hash << 4) + (INT)(*Pointer);
+		Generic = Hash & 0xF0000000L;
+
+		if (Generic != 0)
+			Hash = Hash ^ (Generic >> 24);
+
+		Hash = Hash &~ Generic;
+	}
+
+	return Hash;
+}
