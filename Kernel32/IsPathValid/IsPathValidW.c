58d93e691e62e091f8a98c4c83438510a41de3bd
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-06-04 16:17:33 -0500

    Update IsPathValidW.c

--- Kernel32/IsPathValid/IsPathValidW.c
+++ Kernel32/IsPathValid/IsPathValidW.c
@@ -1,3 +1,24 @@
+/*
+
+IsPathValid function
+
+Summary:
+	Determines if a file exists.
+
+Parameters:
+	A pointer to the full path to the file.
+
+Return value:
+	TRUE if file exists. FALSE if file does not exist or the application is unable to get a HANDLE on the file specified
+	
+Remarks:
+	Use GetLastError() for details on failure
+
+Author:
+smelly__vx | June 4th, 2021
+
+*/
+
 BOOL IsPathValidW(PWCHAR FilePath)
 {
     HANDLE hFile = INVALID_HANDLE_VALUE;
