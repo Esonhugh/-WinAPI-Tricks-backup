e30c319bba2e4113d7a5ad61e136e4db982228b5
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-06-03 22:38:50 -0500

    Update GetCurrentDirectoryAltA.c

--- Kernel32/GetCurrentDirectoryAlt/GetCurrentDirectoryAltA.c
+++ Kernel32/GetCurrentDirectoryAlt/GetCurrentDirectoryAltA.c
@@ -1,3 +1,27 @@
+/*
+
+GetCurrentDirectoryAltA function
+
+Summary:
+	Recreation of GetCurrentDirectory. Relevant for malware dev, eliminates the need to dynamically resolve GetCurrentDirectory from kernel32.dll
+
+Remarks:
+	Precise recreation of GetCurrentDirectory. See MSDN for more details.
+	Upon failure GetLastError() will not work. This function does not set the error code.
+	
+	GetCurrentDirectory: https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-getcurrentdirectory
+	
+	WCharStringToCharString, PEB, GetPeb, and PRTL_USER_PROCESS_PARAMETERS but be defined
+
+	WCharStringToCharString: https://github.com/vxunderground/WinAPI-Tricks/blob/main/Stdio/String%20Conversion/WCharStringToCharString.c
+	PEB + RTL_USER_PROCESS_PARAMETERS: https://github.com/vxunderground/WinAPI-Tricks/blob/main/Headers/RTL_USER_PROCESS_PARAMETERS.h
+	GetPeb: https://github.com/vxunderground/WinAPI-Tricks/blob/main/GetPEB.c
+
+Author:
+smelly__vx | June 3rd, 2021
+
+*/
+
 DWORD GetCurrentDirectoryAltA(DWORD nBufferLength, PCHAR lpBuffer)
 {
 	PPEB Peb = (PPEB)GetPeb();
