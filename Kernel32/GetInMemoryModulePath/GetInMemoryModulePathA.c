7ca8371f2c45900a97b3a8b05da6ed2bad913541
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-06-04 12:35:28 -0500

    Update GetInMemoryModulePathA.c

--- Kernel32/GetInMemoryModulePath/GetInMemoryModulePathA.c
+++ Kernel32/GetInMemoryModulePath/GetInMemoryModulePathA.c
@@ -1,3 +1,27 @@
+/*
+
+GetInMemoryModulePathA function
+
+Summary:
+	Recreation of GetModuleFileName with the first parameter being set to NULL. Relevant for malware dev, eliminates the need to dynamically resolve GetModuleFileName from kernel32.dll
+
+Remarks:
+	Incomplete recreation of GetModuleFileName
+	Upon failure GetLastError() will not work. This function does not set the error code.
+	
+	GetCurrentDirectory: https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulefilenamea
+	
+	WCharStringToCharString, PEB, GetPeb, and PRTL_USER_PROCESS_PARAMETERS but be defined
+
+	WCharStringToCharString: https://github.com/vxunderground/WinAPI-Tricks/blob/main/Stdio/String%20Conversion/WCharStringToCharString.c
+	PEB + RTL_USER_PROCESS_PARAMETERS: https://github.com/vxunderground/WinAPI-Tricks/blob/main/Headers/RTL_USER_PROCESS_PARAMETERS.h
+	GetPeb: https://github.com/vxunderground/WinAPI-Tricks/blob/main/GetPEB.c
+
+Author:
+smelly__vx | June 4th, 2021
+
+*/
+
 #define ERROR_FAILURE_RETURN 0
 
 DWORD GetInMemoryModulePathA(DWORD nBufferLength, PCHAR lpBuffer)
