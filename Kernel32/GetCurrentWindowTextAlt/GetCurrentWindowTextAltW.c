3d40f45b08c2cfa329efafe7769b8acccfed534a
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-06-04 12:50:05 -0500

    Update and rename Kernel32/GetWindowTextAlt/GetWindowTextAltW.c to Kernel32/GetCurrentWindowTextAlt/GetCurrentWindowTextAltW.c

---
+++ Kernel32/GetCurrentWindowTextAlt/GetCurrentWindowTextAltW.c
@@ -0,0 +1,15 @@
+#define ERROR_FAILURE_RETURN 0
+
+DWORD GetCurrentWindowTextAltW(DWORD nBufferLength, PWCHAR lpBuffer)
+{
+	PPEB Peb = (PPEB)GetPeb();
+	PRTL_USER_PROCESS_PARAMETERS ProcessParameters = Peb->ProcessParameters;
+
+	if (nBufferLength < ProcessParameters->WindowTitle.Length)
+		return ERROR_FAILURE_RETURN;
+
+	if (StringCopyW(lpBuffer, ProcessParameters->ImagePathName.Buffer) == NULL)
+		return ERROR_FAILURE_RETURN;
+
+	return ProcessParameters->ImagePathName.Length;
+}
