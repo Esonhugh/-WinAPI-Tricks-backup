e04f105eb5e66ece1030e409620d4aee8597bedf
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-06-30 12:38:05 -0500

    Create WriteWatchVirtualAllocBufferAlloc.c

---
+++ Anti Debug/WriteWatchVirtualAllocBufferAlloc.c
@@ -0,0 +1,32 @@
+BOOL WriteWatchVirtualAllocBufferAlloc(VOID)
+{
+	ULONG_PTR Count = NULL;
+	DWORD Granularity = 0;
+	BOOL bFlag = FALSE;
+	PVOID Address = NULL;
+	PINT Buffer = NULL;
+
+	Address = VirtualAlloc(NULL, (4096 * sizeof(PVOID)), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	if (Address == NULL)
+		return FALSE;
+
+	Buffer = (PINT)VirtualAlloc(NULL, (4096 * 4096), MEM_RESERVE | MEM_COMMIT | MEM_WRITE_WATCH, PAGE_READWRITE);
+	if (Buffer == NULL)
+		goto EXIT_ROUTINE;
+
+	Buffer[0] = 1024;
+	Count = 4096;
+
+	if (!GetWriteWatch(0, Buffer, 4096, Address, &Count, &Granularity))
+		bFlag = Count != 1;
+
+EXIT_ROUTINE:
+
+	if (Buffer)
+		VirtualFree(Buffer, 0, MEM_RELEASE);
+
+	if (Address)
+		VirtualFree(Address, 0, MEM_RELEASE);
+
+	return bFlag;
+}
